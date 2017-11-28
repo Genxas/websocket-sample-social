12afa53f9412443d91d4b3d13851ca92e4f4982c
Christopher Ng <babedroid@gmail.com>
2017-11-28 15:52:49 +0700

    :sparkles: App template

---
+++ index.js
@@ -0,0 +1,29 @@
+const app = require('express')()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+
+var users = []
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/users', (req, res) => {
+    res.send(users)
+})
+
+io.on('connection', (socket) => {
+    socket.on('connected', (data) => {
+        if (data.userId) {
+            users.push(data)
+        }
+    })
+
+    socket.on('postFeed', (data) => {
+        socket.emit('newFeed', data)
+    })
+})
+
+http.listen(3000, () => {
+    console.log('Socket is ready')
+})
